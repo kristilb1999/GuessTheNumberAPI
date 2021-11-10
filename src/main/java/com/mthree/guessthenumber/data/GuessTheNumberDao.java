98a962e7d5d682735736bc753b08066c199c14b2
Kristi B <60147968+kristilb1999@users.noreply.github.com>
2021-11-10 11:40:33 -0500

    Updated interfaces with javadoc descriptions.

--- src/main/java/com/mthree/guessthenumber/data/GuessTheNumberDao.java
+++ src/main/java/com/mthree/guessthenumber/data/GuessTheNumberDao.java
@@ -12,18 +12,68 @@ import java.util.List;
 
 public interface GuessTheNumberDao {
 
+    /**
+     * Returns a list of all the games in the database game table.
+     * Returns null if no games exist in database.
+     *
+     * @return A list of all games found in the database. Or null.
+     */
     List<Game> getAllGames();
 
+    /**
+     * Returns the game associated with the given gameId.
+     * Or null if the game does not exist.
+     *
+     * @param gameId The Id of the specified game.
+     * @return The game associated with the given Id or null.
+     */
     Game getGameById(int gameId);
 
+    /**
+     * Returns the game added to the database, with all
+     * fields completed.
+     *
+     * @param game The game to be added to the database,
+     * no id required.
+     *
+     * @return The game that was added with all fields
+     * completely filled in.
+     */
     Game addGame(Game game);
 
+    /**
+     * Updates the status of the game. Only used when game is
+     * completed and status is changed to finished.
+     *
+     * @param game The game to have its status updated.
+     */
     void updateGame(Game game);
 
+    /**
+     * A list of all rounds associated with a specific game,
+     * retrieved by the gameId or null if none exist.
+     *
+     * @param gameId The Id of the game rounds to look for.
+     * @return The list of rounds played in a specific game.
+     */
     List<Round> getAllRoundsByGameId(int gameId);
 
+    /**
+     * The specific round of a game, retrieved by its own id.
+     * Otherwise, null if the round doesn't exist.
+     *
+     * @param roundId The id of the round to look for.
+     * @return The round by its id or null.
+     */
     Round getRoundById(int roundId);
 
+    /**
+     * Adds a round to the round table and associated it
+     * with the game it was played for.
+     *
+     * @param round The round to be added to the database.
+     * @return The round with all its information.
+     */
     Round addRound(Round round);
 
 }
--- src/main/java/com/mthree/guessthenumber/service/GuessTheNumberServiceLayer.java
+++ src/main/java/com/mthree/guessthenumber/service/GuessTheNumberServiceLayer.java
@@ -12,18 +12,72 @@ import java.util.List;
 
 public interface GuessTheNumberServiceLayer {
 
+    /**
+     * A list of all the games found in the database. Any
+     * games still in progress will not show their answer
+     * to the player.
+     *
+     * @return A list of games in the database.
+     */
     List<Game> getAllGames();
 
+    /**
+     * All rounds associated with a specific game by its
+     * game Id, including the result of that game.
+     *
+     * @param gameId The game Id each round is associated with.
+     * @return The list of rounds for a specific game.
+     */
     List<Round> getAllRoundsByGameId(int gameId);
 
+    /**
+     * Allows user to make a guess for the answer and return
+     * the Round object added for that guess. The result of
+     * the round will be shown to the user with this Round.
+     * Will set the status of the game to finished when the correct
+     * guess is given.
+     *
+     * @param round The gameId and guess to be made.
+     * @return The round that was just added to the database.
+     */
     Round makeGuess(Round round);
 
+    /**
+     * Returns the game associated with a specific game Id.
+     * If the game is in progress, the answer will not be shown.
+     *
+     * @param gameId The game to be returned.
+     * @return The game with or without the answer.
+     */
     Game getGameById(int gameId);
 
+    /**
+     * Adds a game to the database and returns the game with
+     * its own unique id.
+     *
+     * @param game The game that was just added with its
+     * answer and status.
+     * @return The game object that was added with its id.
+     */
     Game addGame(Game game);
 
+    /**
+     * Creates a new game to be added to the database. Game will
+     * be created with an answer made up of 4 unique, random digits.
+     *
+     * @return The id of the game that was added.
+     */
     int newGame();
 
+    /**
+     * Determines the result of the round. Compares the guess with
+     * the actual answer for the game and gives a player a result.
+     *
+     * @param guess The 4 digit guess from the user.
+     * @param answer The 4 digit answer of the game.
+     * @return A String to represent how many digits were correctly
+     * given in the guess, in the form "e:#:p:#"
+     */
     String determineResult(String guess, String answer);
 
 }
